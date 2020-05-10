0722a46a50b58dc6b1dd4a45efe321d3dccb29d3
Juan García Garland <jpgarcia@fing.edu.uy>
2020-05-10 01:11:48 -0300

    update hello world

--- HelloWorld/HelloWorld.ino
+++ HelloWorld/HelloWorld.ino
@@ -50,7 +50,7 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("EZ IMPOZIBLE");
+  lcd.print("Hello, World!");
 }
 
 void loop() {
